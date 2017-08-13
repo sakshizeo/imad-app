535ccde55af1ff8f84306134227405e6ca47dbb9
sakshizeo <30925733+sakshizeo@users.noreply.github.com>
2017-08-13 11:19:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article one ', function (req, res) {
+app.get('/article-one ', function (req, res) {
   res.send('article 1 requested');
 });
 
-app.get('/article two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('article 2 requested');
 });
 
-app.get('/article 3', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('aricle 3 requested');
 });
 
